e05fd87f688a3f2fd55161baedb3f0eef7c38537
Babafemi <babaphemy@gmail.com>
2019-02-25 22:27:38 -0600

    map added

--- src/components/main.js
+++ src/components/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from '../components/NowWhat';
-import Chartview from '../components/ui/chart';
+import Chartview from './Map';
 import Dashview from './Weather';
 
 const Main = () => (
