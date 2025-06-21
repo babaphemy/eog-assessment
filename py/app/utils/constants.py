589d629942d001ad5871a980f47f220ccdf7bf51
Femi Adigun <babaphemy@gmail.com>
2025-06-20 21:21:04 -0500

    chore: remove ununsed constants

--- py/app/utils/constants.py
+++ py/app/utils/constants.py
@@ -1,25 +1,7 @@
 """Constants used throughout the application."""
 
-from enum import Enum
-
 DEFAULT_TAX_RATE = 0.0825
 
 GOOGDIT_PRICE_DIVISOR = 100_000_000
 
 REQUEST_TIMEOUT = 10.0
-
-
-class PlatformURLs:
-    """URLs for different e-commerce platforms."""
-
-    GOOGDIT = "https://googdit.heb-platform-interview.hebdigital-prd.com"
-    APPEDIA = "https://appedia.heb-platform-interview.hebdigital-prd.com"
-    MICROMAZON = "https://micromazon.heb-platform-interview.hebdigital-prd.com"
-
-
-class PlatformType(Enum):
-    """Enumeration of supported platforms."""
-
-    GOOGDIT = "googdit"
-    APPEDIA = "appedia"
-    MICROMAZON = "micromazon"
