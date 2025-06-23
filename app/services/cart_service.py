1264e083a9566ee456efe3c8be5d9557f9613d48
Femi Adigun <babaphemy@gmail.com>
2025-06-23 01:31:53 -0500

    .

--- app/services/cart_service.py
+++ app/services/cart_service.py
@@ -108,7 +108,7 @@ class ShoppingCartManager:
         # Calculate totals
         subtotal = self.calculate_subtotal(final_items)
         tax_total = self.calculate_tax(final_items)
-        grand_total = self.round_to_two_decimals(subtotal + tax_total)
+        grand_total = round(subtotal + tax_total, 2)
 
         return ShoppingCartData(
             subTotal=subtotal, taxTotal=tax_total, grandTotal=grand_total
